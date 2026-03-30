c18ab5a5b36cef943a67aa286466036c0cdc79aa
unknown <seu@email.com>
2026-03-30 18:35:24 -0300

    corrige cloudinary config

--- back-end/cloudinary_config.py
+++ back-end/cloudinary_config.py
@@ -3,7 +3,7 @@ import cloudinary.uploader
 import os
 
 cloudinary.config(
-    cloud_name=os.environ.get("drljztlhv"),
-    api_key=os.environ.get("188239229873561",)
-    api_secret=os.environ.get("66Xh1qeUVMIPXcMQDKVJTgd0Pyo")
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET")
 )
